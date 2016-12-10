3b4dd667a6d8df3bb0ec723f66f794048309f788
matthias <hafner87@gmail.com>
2016-12-10 10:06:55 +0000

    Data2D: add interpolating item getter
    spectrum: calculate dBFS (sine norm.)

--- tools.py
+++ tools.py
@@ -16,15 +16,20 @@ class Data2D(object):
                                     sizing_mode='stretch_both')
         fig.line(self.x, self.y)
         return fig
+        
+    def __getitem__(self, key):
+        return np.interp(key, self.x, self.y)
 
 
 def spectrum(pcm, nfft=None):
+    assert len(pcm.shape) == 1, 'only applicable to 1D data'
     if nfft is None:
         nfft = 1
         while nfft * 2 < pcm.sample_rate and nfft * 2 < len(pcm):
             nfft *= 2
     f, l = scipy.signal.csd(pcm, pcm, pcm.sample_rate, 
                             nperseg=nfft, scaling='spectrum')
+    l = 10 * np.log10(l * 2)  # +3dB, dBFS sine scaling
     return Data2D(f, 'frequency [Hz]', 
-                  l, 'level [dBFS]')
+                  l, 'level [dBFS sine]')
                   
