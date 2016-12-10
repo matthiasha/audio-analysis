e700a549704b470b88dd06b6ee63370f75727e51
matthias <hafner87@gmail.com>
2016-12-10 13:34:57 +0000

    add tests

---
+++ test_audio.py
@@ -0,0 +1,46 @@
+import tools
+import audio
+import numpy as np
+import pytest
+
+TOLERANCE_DB = 0.1  # TODO: decrease
+FILES = {'1000Hz_-3dBFS_3s.wav': (1000, -3),
+         '1000Hz_-6dBFS_3s.wav': (1000, -6),
+         '440Hz_-6dBFS_3s.wav': (440, -6),
+         '2345Hz_-14dBFS_3s.wav': (2345, -14),
+         '5600Hz_-10dBFS_3s.wav': (5600, -10),
+        }
+
+
+def lin(dbfs):
+    return 10 ** (dbfs / 20)
+
+
+@pytest.mark.parametrize('file', FILES.keys())
+def test_pcmarray(file):
+    hz, dbfs = FILES[file]
+    ch = 0
+    pcm = audio.PCMArray(file)[ch]
+    assert min(abs(pcm)) < lin(TOLERANCE_DB)
+    assert abs(max(pcm) - lin(dbfs)) < TOLERANCE_DB
+    
+
+@pytest.mark.parametrize('file', FILES.keys())
+def test_spectrum(file):
+    TOLERANCE_DB = 1.5  # TODO: decrease
+    hz, dbfs = FILES[file]
+    ch = 0
+    pcm = audio.PCMArray(file)[ch]
+    s = tools.spectrum(pcm)
+    assert abs(s[hz] - dbfs) < TOLERANCE_DB
+    
+    
+@pytest.mark.parametrize('file', FILES.keys())
+def test_power(file):
+    hz, dbfs = FILES[file]
+    ch = 0
+    pcm = audio.PCMArray(file)[ch]
+    p = tools.power(pcm, window=1023)
+    assert all(abs(p.y - dbfs) < TOLERANCE_DB + .1)
+    assert abs(p.y.mean() - dbfs) < TOLERANCE_DB
+    
