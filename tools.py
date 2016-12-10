3081e174686f4b342f211200f494db7ef19d1d23
matthias <hafner87@gmail.com>
2016-12-10 13:34:32 +0000

    add power function

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import scipy.signal
 import numpy as np
 import bokeh.plotting
+import bokeh.models
 
 
 class Data2D(object):
@@ -9,16 +10,31 @@ class Data2D(object):
         self.y = y
         self.xlabel = xlabel
         self.ylabel = ylabel
+        self._x2_factor = None
+        self._x2_label = None
         
     def plot(self):
         fig = bokeh.plotting.figure(x_axis_label=self.xlabel,
                                     y_axis_label=self.ylabel, 
                                     sizing_mode='stretch_both')
+        if self._x2_factor is not None:
+            x2_ax = bokeh.models.LinearAxis(axis_label=self._x2_label)
+            js_code = "Math.round(tick * %f * 100) / 100" % self._x2_factor
+            x2_ax.formatter = bokeh.models.FuncTickFormatter(code=js_code)
         fig.line(self.x, self.y)
         return fig
         
     def __getitem__(self, key):
         return np.interp(key, self.x, self.y)
+        
+    def add_x_axis(self, factor, label):
+        assert self._x2_factor is None, 'only 2 x-axis possible' 
+        self._x2_factor = factor
+        self._x2_label = label
+
+
+def _dbfs(sq):
+    return 10 * np.log10(sq * 2)  # +3dB, dBFS sine scaling
 
 
 def spectrum(pcm, nfft=None):
@@ -27,9 +43,24 @@ def spectrum(pcm, nfft=None):
         nfft = 1
         while nfft * 2 < pcm.sample_rate and nfft * 2 < len(pcm):
             nfft *= 2
-    f, l = scipy.signal.csd(pcm, pcm, pcm.sample_rate, 
+    f, l_sq = scipy.signal.csd(pcm, pcm, pcm.sample_rate, 
                             nperseg=nfft, scaling='spectrum')
-    l = 10 * np.log10(l * 2)  # +3dB, dBFS sine scaling
+    l = _dbfs(l_sq) 
     return Data2D(f, 'frequency [Hz]', 
                   l, 'level [dBFS sine]')
+        
                   
+def power(pcm, window=255):
+    assert len(pcm.shape) == 1, 'only applicable to 1D data'
+    if len(pcm) % window:
+        # truncate samples to be multiple of window
+        pcm = pcm[:-(len(pcm) % window)]
+    reshaped = pcm.reshape(-1, window)
+    i = np.arange(len(pcm)).reshape(-1, window).mean(1)
+    # calculate power per window
+    p = _dbfs(np.mean(reshaped ** 2, 1)) 
+    d2d = Data2D(i, 'samples', 
+                 p, 'level [dBFS sine]')
+    d2d.add_x_axis('time [s]', 1 / pcm.sample_rate)
+    return d2d
+    
